983a5615ce4f566d2c592e2125193a9d000208b4
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2023-09-22 14:37:55 +0200

    1.1 API : read

--- exercises/1.1/routes/index.js
+++ exercises/1.1/routes/index.js
@@ -1,9 +1,10 @@
+const { json } = require('express');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('/index.hbs', { title: 'Express' });
+  res.render('/index');
 });
 
 module.exports = router;
