e2b0fe90be2778d56e9356f2aed85813f52dd1d8
Gabriel Paquet <gabriel.paquet@student.vinci.be>
2023-10-13 15:42:35 +0200

    Debut bonus 2.3.1

--- exercises/2.3.1/index.js
+++ exercises/2.3.1/index.js
@@ -1,12 +1,15 @@
 const souhait = document.querySelector("#souhait");
 const divForm = document.querySelector("#divForm");
 const divSouhait = document.querySelector("#divSouhait");
+const newButton = document.querySelector("#comeback");
 const onSubmit = (e) => {
 
     e.preventDefault();
 
     divForm.style.display = "none";
     divSouhait.innerText = souhait.value;
+
+    newButton.display.display = "flex";
 };
 
 form.addEventListener("submit", onSubmit);
